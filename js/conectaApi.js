39b36b8a953784d480738f07004e22418dd34553
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-03-03 12:39:32 -0300

    update

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -19,11 +19,20 @@ async function criaVideo(titulo, descricao, url, imagem) {
     }),
   });
 
+  const conexaoConvertida = conexao.json();
+
+  return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+  const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
   const conexaoConvertida = await conexao.json();
+
   return conexaoConvertida;
 }
 
 export const conectaApi = {
   listaVideos,
   criaVideo,
+  buscaVideo,
 };
